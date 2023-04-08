ae42056713bbf768e53b9799e3f88de0b514fa3f
faez <20587852@students.ltu.edu.au>
2023-04-08 12:03:58 +1000

    Added Home and Registration Page

---
+++ app/src/main/java/com/example/mobileappdev/HomePage.java
@@ -0,0 +1,32 @@
+package com.example.mobileappdev;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import android.os.Bundle;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
---
+++ app/src/main/java/com/example/mobileappdev/RegistrationPage.java
@@ -0,0 +1,47 @@
+package com.example.mobileappdev;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import android.os.Bundle;
+
+public class RegistrationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page);
+
+        Button button1;
+        Button button2;
+
+        button1 = findViewById(R.id.button5);
+        button2 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationPage.this, MainActivity.class));
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationPage.this,HomePage.class));
+            }
+        });
+    }
+}
