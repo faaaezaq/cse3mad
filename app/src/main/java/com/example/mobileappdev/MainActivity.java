8b92878329f425ad540d5c928c5714102c3b3480
faez <20587852@students.ltu.edu.au>
2023-04-08 14:03:57 +1000

    Updated Home and registration page

--- app/src/main/java/com/example/mobileappdev/MainActivity.java
+++ app/src/main/java/com/example/mobileappdev/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.mobileappdev;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.view.MenuItem;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = findViewById(R.id.btn1);
-        button2 = findViewById(R.id.btn2);
+        button1 = findViewById(R.id.button_login);
+        button2 = findViewById(R.id.button);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegistrationPage.class));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HomePage.class));
             }
         });
     }
