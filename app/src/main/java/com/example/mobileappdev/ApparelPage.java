3fc7b6f3e2a7fb18c307b3ab232f0ce99bd4aede
kritistha <kritistha020@gmail.com>
2023-04-25 14:10:02 +1000

    made navbar generic

--- app/src/main/java/com/example/mobileappdev/ApparelPage.java
+++ app/src/main/java/com/example/mobileappdev/ApparelPage.java
@@ -3,6 +3,8 @@ package com.example.mobileappdev;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class ApparelPage extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class ApparelPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apparel);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationBar);
+        NavigationbarClass.setupBottomNavigation(bottomNavigationView, this);
 
 
     }
